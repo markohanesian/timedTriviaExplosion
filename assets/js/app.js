5439b23780d7d1201c1a0dd1f935497ea87e4d75
Mark <mso872@gmail.com>
2020-02-11 18:58:28 -0800

    fixed hourglass container

--- assets/js/app.js
+++ assets/js/app.js
@@ -69,9 +69,6 @@ function createMaterials() {
 }
 
 function createGeometries() {
-  // const nose = new THREE.CylinderBufferGeometry(0.75, 0.75, 3, 12);
-
-  // const cabin = new THREE.BoxBufferGeometry( 2, 2.25, 1.5 );
 
   const top = new THREE.CylinderBufferGeometry(0.9, 0.3, 1.5);
 
@@ -79,16 +76,10 @@ function createGeometries() {
 
   bottom.applyMatrix(new THREE.Matrix4().makeScale(-1, -1, 1));
 
-  // we can reuse a single cylinder geometry for all 4 wheels
-  // const wheel = new THREE.CylinderBufferGeometry(0.4, 0.4, 1.75, 16);
-  // wheel.rotateX(Math.PI / 2);
-
   return {
-    // nose,
-    // cabin,
+    
     top,
     bottom
-    // wheel
   };
 }
 
