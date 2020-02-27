ebea478dd59c5a3fda10238bcae6ed78585487b4
ar <56378858+arumbaua366@users.noreply.github.com>
2020-02-26 18:08:56 -0800

    decoded html entity encoding

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,16 +2,38 @@ var allQuestions = [];
 
 // pulls all 50 trivia questions from opentdb API
 function questionDisplay() {
-  var queryURL =
-    "https://opentdb.com/api.php?amount=50&type=boolean";
+  // var queryURL =
+  //   "https://opentdb.com/api.php?amount=50&type=boolean";
 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    allQuestions = response;
-    answerCorrect = respAnsOne;
-    answerWrong = respAnsTwo;
+  // $.ajax({
+  //   url: queryURL,
+  //   method: "GET"
+  // }).then(function(response) {
+  //   allQuestions = response;
+  //   answerCorrect = respAnsOne;
+  //   answerWrong = respAnsTwo;
+
+
+  $.get(
+    "https://opentdb.com/api.php?amount=50&type=boolean&encode=url3986",
+    function(res) {
+      // console.log(res.results)
+      let questions = res.results.map(elem => {
+        console.log("before:", elem.question)
+        elem.question = decodeURIComponent(elem.question);
+        console.log("after:", elem.question)
+        elem.correct_answer = decodeURIComponent(elem.correct_answer);
+        elem.incorrect_answers = elem.incorrect_answers.map(incorrect =>
+          decodeURIComponent(incorrect)
+        );
+          console.log("after:", elem)
+        return elem;
+      });
+      
+      console.log(questions)
+      //alert(questions);
+      var response = {}
+     response.results = questions
 
     //loops through each question
     for (var i = 0; i < response.results.length; i++) {
@@ -63,15 +85,19 @@ $.get(
   function(res) {
     // console.log(res.results)
     let questions = res.results.map(elem => {
+      console.log("before:", elem.question)
       elem.question = decodeURIComponent(elem.question);
+      console.log("after:", elem.question)
       elem.correct_answer = decodeURIComponent(elem.correct_answer);
       elem.incorrect_answers = elem.incorrect_answers.map(incorrect =>
         decodeURIComponent(incorrect)
       );
-
+        console.log("after:", elem)
       return elem;
     });
-    alert(elem.questions);
+    
+    console.log(questions)
+    //alert(questions);
   }
 );
 
@@ -176,94 +202,4 @@ $("img").explode({
 // not working
 function highScoresDisplay() {}
 
-// var WEBGL = {
 
-// 	isWebGLAvailable: function () {
-
-// 		try {
-
-// 			var canvas = document.createElement( 'canvas' );
-// 			return !! ( window.WebGLRenderingContext && ( canvas.getContext( 'webgl' ) || canvas.getContext( 'experimental-webgl' ) ) );
-
-// 		} catch ( e ) {
-
-// 			return false;
-
-// 		}
-
-// 	},
-
-// 	isWebGL2Available: function () {
-
-// 		try {
-
-// 			var canvas = document.createElement( 'canvas' );
-// 			return !! ( window.WebGL2RenderingContext && canvas.getContext( 'webgl2' ) );
-
-// 		} catch ( e ) {
-
-// 			return false;
-
-// 		}
-
-// 	},
-
-// 	getWebGLErrorMessage: function () {
-
-// 		return this.getErrorMessage( 1 );
-
-// 	},
-
-// 	getWebGL2ErrorMessage: function () {
-
-// 		return this.getErrorMessage( 2 );
-
-// 	},
-
-// 	getErrorMessage: function ( version ) {
-
-// 		var names = {
-// 			1: 'WebGL',
-// 			2: 'WebGL 2'
-// 		};
-
-// 		var contexts = {
-// 			1: window.WebGLRenderingContext,
-// 			2: window.WebGL2RenderingContext
-// 		};
-
-// 		var message = 'Your $0 does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">$1</a>';
-
-// 		var element = document.createElement( 'div' );
-// 		element.id = 'webglmessage';
-// 		element.style.fontFamily = 'monospace';
-// 		element.style.fontSize = '13px';
-// 		element.style.fontWeight = 'normal';
-// 		element.style.textAlign = 'center';
-// 		element.style.background = '#fff';
-// 		element.style.color = '#000';
-// 		element.style.padding = '1.5em';
-// 		element.style.width = '400px';
-// 		element.style.margin = '5em auto 0';
-
-// 		if ( contexts[ version ] ) {
-
-// 			message = message.replace( '$0', 'graphics card' );
-
-// 		} else {
-
-// 			message = message.replace( '$0', 'browser' );
-
-// 		}
-
-// 		message = message.replace( '$1', names[ version ] );
-
-// 		element.innerHTML = message;
-
-// 		return element;
-
-// 	}
-
-// };
-
-//export { WEBGL };
